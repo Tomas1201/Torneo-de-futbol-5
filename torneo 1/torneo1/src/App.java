564f508e69c7d335acf85c7d9bb64849a9aed5aa
Tomas Acosta <tomasacosta53@protonmail.com>
2023-06-24 20:18:00 -0300

    Ordene el codigo con ctrl + k, ctrl + f

--- torneo 1/torneo1/src/App.java
+++ torneo 1/torneo1/src/App.java
@@ -1,34 +1,34 @@
 
 import java.util.Scanner;
-public class App {
-static Menus menu = new Menus();
-static PlantillaE p = new PlantillaE();
-static Inscripcion ins = new Inscripcion();
-static Equipos equi = new Equipos();
-static fech fix = new fech();
 
+public class App {
+    static Menus menu = new Menus();
+    static PlantillaE p = new PlantillaE();
+    static Inscripcion ins = new Inscripcion();
+    static Equipos equi = new Equipos();
+    static fech fix = new fech();
 
     public static void main(String[] args) throws Exception {
-    Byte opcion;
+        Byte opcion;
         Scanner sc = new Scanner(System.in);
         menu.cartelera();
         opcion = sc.nextByte();
         sc.nextLine();
 
-        switch(opcion){
-        case 1:
-            ins.InscripEquipo();
-            break;
-        case 2:
-        fix.fixture();
-        break;
-        case 3:
-        p.plantillae();
-        case 0:
-        break;
-        default:
-        System.out.println("Opcion invalida");
+        switch (opcion) {
+            case 1:
+                ins.InscripEquipo();
+                break;
+            case 2:
+                fix.fixture();
+                break;
+            case 3:
+                p.plantillae();
+            case 0:
+                break;
+            default:
+                System.out.println("Opcion invalida");
         }
-      sc.close();
+        sc.close();
     }
 }
--- torneo 1/torneo1/src/Equipos.java
+++ torneo 1/torneo1/src/Equipos.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 
 public class Equipos {
-      private byte CantidadJugadores;
-      private String Capitan;
-      private String Subcapitan;
-      private String NombreEquipo;
-      private String Comodines;
-      private Boolean Eliminado;
-      private Boolean Document1;
-      private Boolean Document2;
-      private Boolean InsJuga = true;
-     final int integrantesMaximos = 14;
-     final int integrantesMinimos = 9;
-     final int MaxComodines = 2;
-          
+    private byte CantidadJugadores;
+    private String Capitan;
+    private String Subcapitan;
+    private String NombreEquipo;
+    private String Comodines;
+    private Boolean Eliminado;
+    private Boolean Document1;
+    private Boolean Document2;
+    private Boolean InsJuga = true;
+    final int integrantesMaximos = 14;
+    final int integrantesMinimos = 9;
+    final int MaxComodines = 2;
 
-     //static Jugadores Juga = new Jugadores();
-     static Scanner sc = new Scanner(System.in);
-     
+    // static Jugadores Juga = new Jugadores();
+    static Scanner sc = new Scanner(System.in);
+
     Jugadores[] jugadores2 = new Jugadores[CantidadJugadores];
 
+    // Setter y Getter para InsJuga
+    public void setInsJuga(Boolean InsJuga) {
+        this.InsJuga = InsJuga;
+    }
 
-    //Setter y Getter para InsJuga
-        public void setInsJuga(Boolean InsJuga){
-            this.InsJuga = InsJuga;
-        }
-
-        public Boolean getInsJuga(){
-            return InsJuga;
-        }
+    public Boolean getInsJuga() {
+        return InsJuga;
+    }
 
     // Setter y Getter para cantidadJugadores
     public void setCantidadJugadores(byte cantidadJugadores) {
@@ -84,47 +82,48 @@ public class Equipos {
         return Eliminado;
     }
 
-    //Setter y Getter para Documen1
-    public void setDocument1(Boolean Document1){
+    // Setter y Getter para Documen1
+    public void setDocument1(Boolean Document1) {
         this.Document1 = Document1;
     }
 
-    public Boolean getDocument1(){
+    public Boolean getDocument1() {
         return Document1;
     }
 
-    //Setter y Getter para Documen2
-    public void setDocument2(Boolean Document2){
+    // Setter y Getter para Documen2
+    public void setDocument2(Boolean Document2) {
         this.Document2 = Document2;
     }
 
-    public Boolean getDocument2(){
+    public Boolean getDocument2() {
         return Document2;
     }
 
-//Se ingresan los nombres de los jugadores
-   
-public void NombreJugador(){
-for(int i =0;i < CantidadJugadores;i++){
-jugadores2[i] = new Jugadores();
-System.out.println("Ingrese el nombre del "+(i+1)+" Jugador: ");
-jugadores2[i].setNombreJugador(sc.nextLine()); 
-System.out.println("Ingrese el DNI de "+jugadores2[i].getNombrejugador()+": " );
-        jugadores2[i].setDNI(sc.nextInt());
-        sc.nextLine();
-System.out.println("Ingrese la posicion del jugador: ");
-        jugadores2[i].setPocision(sc.nextLine());
+    // Se ingresan los nombres de los jugadores
+
+    public void NombreJugador() {
+        for (int i = 0; i < CantidadJugadores; i++) {
+            jugadores2[i] = new Jugadores();
+            System.out.println("Ingrese el nombre del " + (i + 1) + " Jugador: ");
+            jugadores2[i].setNombreJugador(sc.nextLine());
+            System.out.println("Ingrese el DNI de " + jugadores2[i].getNombrejugador() + ": ");
+            jugadores2[i].setDNI(sc.nextInt());
+            sc.nextLine();
+            System.out.println("Ingrese la posicion del jugador: ");
+            jugadores2[i].setPocision(sc.nextLine());
+
+        }
 
     }
 
- }
-//Imprime los nombres de los jugadores
-public void Decirnombre(){
-    for(int i = 0; i < jugadores2.length;i++){
+    // Imprime los nombres de los jugadores
+    public void Decirnombre() {
+        for (int i = 0; i < jugadores2.length; i++) {
 
-System.out.println("El nombre del jugador es: "+jugadores2[i].getNombrejugador());
+            System.out.println("El nombre del jugador es: " + jugadores2[i].getNombrejugador());
+        }
+
     }
-
-}
 
 }
