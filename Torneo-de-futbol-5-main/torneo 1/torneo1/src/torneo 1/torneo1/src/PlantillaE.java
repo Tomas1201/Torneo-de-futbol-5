8459deea6aa33bdbcff12b2798ec263e627ff05e
Thomas Gonzalez <136638210+ThomasUGonzalez@users.noreply.github.com>
2023-06-28 19:36:00 -0300

    NUEVO
    fase de eliminacion giles

---
+++ Torneo-de-futbol-5-main/torneo 1/torneo1/src/torneo 1/torneo1/src/PlantillaE.java
@@ -0,0 +1,16 @@
+public class PlantillaE {
+    static String hola;
+    Jugadores juga = new Jugadores();
+    public void plantillae(){
+         
+    System.out.println("------------------------------------------");
+    System.out.println("                   "+ hola +"             ");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    }}
+    
+
+    
+   
