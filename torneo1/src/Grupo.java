ea624488df54f0f7991374c94c78c94bdf5ab4b6
MirkoMateljan <136759111+MirkoMateljan@users.noreply.github.com>
2023-06-28 19:17:59 -0300

    Actualización más nueva
    Añado dos variables a la clase Grupo donde se guardan el equipo ganador y el segundo lugar del grupo, y las clases GanadorGrupo y SegundoLugarGrupo

--- torneo1/src/Grupo.java
+++ torneo1/src/Grupo.java
@@ -2,6 +2,14 @@ public class Grupo {
     Equipos equipos = new Equipos();
     private Equipos[] equipo;
     private String nombreGrupo;
+    private Equipos equipoGanador;
+    private Equipos equipoSegundo;
+    public Equipos getEquipoSegundo() {
+        return equipoSegundo;
+    }
+    public void setEquipoSegundo(Equipos equipoSegundo) {
+        this.equipoSegundo = equipoSegundo;
+    }
     public void setEquipo(Equipos[] equipo) {
         this.equipo = equipo;
     }
@@ -13,5 +21,11 @@ public class Grupo {
     }
     public String getNombreGrupo(){
         return nombreGrupo;
+    }
+        public Equipos getEquipoGanador() {
+        return equipoGanador;
+    }
+    public void setEquipoGanador(Equipos equipoGanador) {
+        this.equipoGanador = equipoGanador;
     }
 }
